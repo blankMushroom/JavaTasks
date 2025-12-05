ad006e99da48f1746ea8f8e550f412ae1c242cd1
blankMushroom <karapunder@gmail.com>
2025-12-05 22:24:00 +0300

    Added literally a single line of code

---
+++ src/com/company/literallyasinglelineofcode/LiterallyASingleLineOfCode.java
@@ -0,0 +1,8 @@
+package com.company.literallyasinglelineofcode;
+
+public class LiterallyASingleLineOfCode {
+    public static void main(String[] args){
+        int a = 2;
+        int i = (a == 1) ? 1 : ((a == 2) ? 2 : 3);
+    }
+}
